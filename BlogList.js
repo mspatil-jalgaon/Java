271c2c56dd877be6894613e189ae08c23b8cfa29
Mukesh Patil <mspatil.jalgaon@gmail.com>
2025-06-26 10:05:46 +0530

    Update BlogList.js

--- BlogList.js
+++ BlogList.js
@@ -12,35 +12,35 @@ const blogPosts = [
     file: "posts/post2.md",
     date: "2025-06-20",
     category: "Programming"
-  }
+  },
   {
     id: 3,
     title: "Java Input and Output Example",
     file: "posts/post3.md",
     date: "2025-06-20",
     category: "Programming"
-  }
+  },
   {
     id: 4,
     title: "Java Addition Program",
     file: "posts/post4.md",
     date: "2025-06-20",
     category: "Programming"
-  }
+  },
   {
     id: 5,
     title: "Square and Cube of a Number",
     file: "posts/post5.md",
     date: "2025-06-20",
     category: "Programming"
-  }
+  },
   {
     id: 6,
     title: "Area of a Circle",
     file: "posts/post6.md",
     date: "2025-06-20",
     category: "Programming"
-  }
+  },
   {
     id: 7,
     title: "Division of Two Numbers",
